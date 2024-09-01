d3ed23aabb2b38314238cd6a47b75d9585424347
Bayronjrc <92125638+Bayronjrc@users.noreply.github.com>
2024-09-01 13:23:09 -0600

    agregamos una variable global

---
+++ Programa/data_processing.h
@@ -0,0 +1,9 @@
+#ifndef DATA_PROCESSING_H
+#define DATA_PROCESSING_H
+
+#include "data_structures.h"
+
+void completarDatosFaltantes(ListaVentas *listaVentas);
+void eliminarDuplicados(ListaVentas *listaVentas);
+
+#endif
